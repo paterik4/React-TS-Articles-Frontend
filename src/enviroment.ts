79a70c8618fc2157146fbb3e3239a8e0939c2540
Keszei Patrik <paterik4@gmail.com>
2021-11-15 17:07:25 +0100

    #18 deploy changes

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,4 +1,3 @@
-export const API_URL =
-    'https://mysterious-scrubland-92173.herokuapp.com:3000/api/'
+export const API_URL = 'https://mysterious-scrubland-92173.herokuapp.com/api/'
 
 export const adminUsers = ['paterik4', 'admin123', 'IDDQD', 'iddqd']
